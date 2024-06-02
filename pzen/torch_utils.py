4bc3df67f4da5fbc809d4943f81f1302f913e8fd
Fabian Keller <github.100.fkeller@spamgourmet.com>
2024-06-02 17:13:08 +0200

    more torch utils

--- pzen/torch_utils.py
+++ pzen/torch_utils.py
@@ -1,5 +1,7 @@
 from typing import Iterator, TypeVar
 
+import torch
+from torch.testing._comparison import assert_close as assert_close_orig
 from torch.utils.data import DataLoader
 
 T = TypeVar("T")
@@ -11,3 +13,21 @@ def iter_dataloader(data_loader: DataLoader[T]) -> Iterator[T]:
     """
     for batch in data_loader:
         yield batch
+
+
+def inverse_sigmoid(x: torch.Tensor) -> torch.Tensor:
+    # https://stackoverflow.com/a/77031136/1804173
+    return torch.log(x) - torch.log(1 - x)
+
+
+def assert_close(
+    actual: torch.Tensor,
+    expected: torch.Tensor | object,
+    rtol: float | None = None,
+    atol: float | None = None,
+) -> None:
+    if not isinstance(expected, torch.Tensor):
+        expected = torch.tensor(expected)
+    print(f"\nActual:   {actual}")
+    print(f"Expected: {expected}")
+    assert_close_orig(actual, expected, rtol=rtol, atol=atol)
